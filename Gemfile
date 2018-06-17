072e38ff14063e2bb889684646a07cd63a459179
Ahmad Hassan <ahmad.hassan612@gmail.com>
2018-06-17 11:43:26 +0200

    gitlab-ci.yml

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 gem 'rspec'
 gem 'aws-sdk-s3'
-gem 'inifile'
 gem 'capybara'
 gem 'selenium-webdriver'
 gem 'chromedriver-helper'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require 'aws-sdk-s3'
-require 'inifile'
 require 'open-uri'
 require 'open3'
 require 'capybara/rspec'
@@ -62,7 +61,12 @@ RSpec.configure do |config|
 end
 
 def pod_name
-  @pod ||= `kubectl get pod -l app=task-runner -o jsonpath="{.items[0].metadata.name}"`
+  if ENV['RELEASE_NAME']
+    release_filter="-l #{ENV['RELEASE_NAME']}"
+  end
+  release_filter ||= ""
+
+  @pod ||= `kubectl get pod #{release_filter} -l app=task-runner -o jsonpath="{.items[0].metadata.name}"`
 end
 
 def ensure_backups_on_object_storage
@@ -78,13 +82,19 @@ def ensure_backups_on_object_storage
   end
 end
 
-s3cfg=IniFile.load("#{Dir.home}/.s3cfg")["default"]
+if ENV['S3_CONFIG_PATH']
+  s3_access_key = File.read("#{ENV['S3_CONFIG_PATH']}/accesskey")
+  s3_secret_key = File.read("#{ENV['S3_CONFIG_PATH']}/secretkey")
+end
+
+s3_access_key ||= ENV['S3_ACCESS_KEY']
+s3_secret_key ||= ENV['S3_SECRET_KEY']
 
 conf = {
-  region: s3cfg['region'] || 'us-east-1',
-  access_key_id: s3cfg['access_key'],
-  secret_access_key: s3cfg['secret_key'],
-  endpoint: s3cfg['website_endpoint'],
+  region: ENV['S3_REGION'] || 'us-east-1',
+  access_key_id: s3_access_key,
+  secret_access_key: s3_secret_key,
+  endpoint: ENV['S3_ENDPOINT'],
   force_path_style: true
 }
 
