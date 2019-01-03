39275bb438b68b034d18d228cd9faaacd5d7fcbb
DJ Mountney <david@twkie.net>
2019-01-02 18:29:32 -0800

    Flag the feature tests as feature types

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,4 +43,8 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.define_derived_metadata(file_path: %r{/spec/features/}) do |metadata|
+    metadata[:type] = :feature
+  end
 end
