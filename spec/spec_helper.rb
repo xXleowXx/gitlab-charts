5cd14c57f515bbb00e15c393d673eae7329061a8
DJ Mountney <david@twkie.net>
2019-01-02 16:11:20 -0800

    Add screenshot handling for our driver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,15 @@ Capybara.register_driver :headless_chrome do |app|
     desired_capabilities: capabilities
 end
 
+# Keep only the screenshots generated from the last failing test suite
+Capybara::Screenshot.prune_strategy = :keep_last_run
+
+# From https://github.com/mattheworiordan/capybara-screenshot/issues/84#issuecomment-41219326
+Capybara::Screenshot.register_driver(:headless_chrome) do |driver, path|
+  driver.browser.save_screenshot(path)
+end
+
+
 Capybara.configure do |config|
   config.run_server = false
   config.default_driver = :headless_chrome
