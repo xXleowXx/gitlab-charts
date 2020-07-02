bb370a445005c4070c3ae416e21b55b8bc010ede
Gerard Hickey <ghickey@gitlab.com>
2020-07-01 23:28:49 -0700

    More complete spec test for workhorse config

--- spec/configuration/workhorse_toml_spec.rb
+++ spec/configuration/workhorse_toml_spec.rb
@@ -10,23 +10,21 @@ describe 'Workhorse TOML configuration', :focus => true do
   describe 'gitlab.webservice.workhorse.storage.enabled' do
     context 'when true' do
       let(:values) do
-        {
-          'gitlab' => {
-            'webservice' => {
-              'workhorse' => {
-                'storage' => {
-                  'enabled' => true
-                }
-              }
-            }
-          }
-        }.merge(default_values)
+        YAML.load(<<~EOS
+          gitlab:
+            webservice:
+              workhorse:
+                storage:
+                  enabled: true
+                  provider: AWS
+          EOS
+        ).merge(default_values)
       end
 
       it 'enables workhorse storage' do
         t = HelmTemplate.new(values)
         expect(t.exit_code).to eq(0)
-        expect(t.dig('ConfigMap/test-workhorse-config','data','workhorse-config.toml.erb')).to include("[object_storage]\nenabled = true")
+        expect(t.dig('ConfigMap/test-workhorse-config','data','workhorse-config.toml.erb')).to include("[object_storage]\nenabled = \"true\"")
       end
 
       it 'uses "AWS" provider' do
@@ -35,41 +33,39 @@ describe 'Workhorse TOML configuration', :focus => true do
         expect(t.dig('ConfigMap/test-workhorse-config','data','workhorse-config.toml.erb')).to include("provider = \"AWS\"")
       end
 
-      context 'and object storage secrets not specified' do
-
+      it 'specifies object storage secrets' do
+        t = HelmTemplate.new(values)
+        expect(t.exit_code).to eq(0)
+        expect(t.dig('ConfigMap/test-workhorse-config','data','workhorse-config.toml.erb')).to include("aws_access_key_id = \"")
+        expect(t.dig('ConfigMap/test-workhorse-config','data','workhorse-config.toml.erb')).to include("aws_secret_access_key = \"")
       end
     end
 
     context 'when false' do
       let(:values) do
-        {
-          'gitlab' => {
-            'webservice' => {
-              'workhorse' => {
-                'storage' => {
-                  'enabled' => false
-                }
-              }
-            }
-          }
-        }.merge(default_values)
+        YAML.load(<<~EOS
+          gitlab:
+            webservice:
+              workhorse:
+                storage:
+                  enabled: false
+                  provider: AWS
+          EOS
+        ).merge(default_values)
       end
 
       it 'disables workhorse storage' do
         t = HelmTemplate.new(values)
         expect(t.exit_code).to eq(0)
-        expect(t.dig('ConfigMap/test-workhorse-config','data','workhorse-config.toml.erb')).to include("[object_storage]\nenabled = false")
+        expect(t.dig('ConfigMap/test-workhorse-config','data','workhorse-config.toml.erb')).to include("[object_storage]\nenabled = \"false\"")
       end
 
-      context 'and object storage secrets not specified' do
-
+      it 'does not specify object storage secrets' do
+        t = HelmTemplate.new(values)
+        expect(t.exit_code).to eq(0)
+        expect(t.dig('ConfigMap/test-workhorse-config','data','workhorse-config.toml.erb')).not_to include("aws_access_key_id = \"")
+        expect(t.dig('ConfigMap/test-workhorse-config','data','workhorse-config.toml.erb')).not_to include("aws_secret_access_key = \"")
       end
     end
-
   end
-
-  describe 'gitlab.webservice.workhorse.storage.password' do
-
-  end
-
 end
