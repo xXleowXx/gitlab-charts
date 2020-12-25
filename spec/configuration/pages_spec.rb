502748f0603ef83063ed8708287c1ee9c0838939
Balasankar "Balu" C <balasankarc@autistici.org>
2020-12-25 11:31:33 +0530

    Enhance Pages specs to cover custom domains
    Signed-off-by: Balasankar "Balu" C <balasankarc@autistici.org>

--- spec/configuration/pages_spec.rb
+++ spec/configuration/pages_spec.rb
@@ -171,8 +171,8 @@ describe 'GitLab Pages' do
                 'host' => 'mycustompages.com',
                 'port' => 123,
                 'https' => false,
-                'externalHttp' => true,
-                'externalHttps' => true,
+                'externalHttp' => %w[1.2.3.4],
+                'externalHttps' => %w[1.2.3.4],
                 'artifactsServer' => false,
                 'objectStore' => {
                   'enabled' => true,
@@ -411,6 +411,346 @@ describe 'GitLab Pages' do
           MSG
 
           expect(config_data).to eq default_content
+        end
+      end
+    end
+
+    describe 'customDomains' do
+      subject(:gitlab_yml_data) do
+        YAML.safe_load(pages_enabled_template.dig('ConfigMap/test-webservice', 'data', 'gitlab.yml.erb'))
+      end
+
+      subject(:pages_config_data) do
+        pages_enabled_template.dig('ConfigMap/test-gitlab-pages', 'data', 'config.erb')
+      end
+
+      context 'when not enabled' do
+        describe 'gitlab.yml file' do
+          it 'sets externalHTTP and externalHTTPS to false' do
+            expect(gitlab_yml_data['production']['pages']['external_http']).to be false
+            expect(gitlab_yml_data['production']['pages']['external_https']).to be false
+          end
+        end
+
+        describe 'pages configuration' do
+          it 'does not expose listen-http, listen-https, root-cert or root-key' do
+            expect(pages_config_data).not_to match(/listen-http=/)
+            expect(pages_config_data).not_to match(/listen-https=/)
+            expect(pages_config_data).not_to match(/root-cert=/)
+            expect(pages_config_data).not_to match(/root-key=/)
+          end
+
+          it 'exposes listen-proxy correctly' do
+            expect(pages_config_data).to match(/listen-proxy=0.0.0.0:8090/)
+          end
+        end
+
+        describe 'pages-custom-domain service' do
+          it 'is not enabled' do
+            expect(pages_enabled_template.dig('Service/test-gitlab-pages-custom-domains')).to be nil
+          end
+        end
+
+        describe 'pages TLS secret' do
+          it 'is not mounted on Pages pod' do
+            pages_secret_mounts = pages_enabled_template.projected_volume_sources(
+              'Deployment/test-gitlab-pages',
+              'init-pages-secrets'
+            )
+
+            tls_mount = pages_secret_mounts.find { |mount| mount['secret']['name'] == 'test-pages-tls' }
+
+            expect(tls_mount).to be_nil
+          end
+        end
+      end
+
+      context 'when only HTTP custom domains are enabled' do
+        let(:pages_enabled_values) do
+          {
+            'global' => {
+              'pages' => {
+                'enabled' => true,
+                'externalHttp' => %w[1.2.3.4]
+              }
+            }
+          }
+        end
+
+        describe 'gitlab.yml file' do
+          it 'sets externalHTTP to true and externalHTTPS to false' do
+            expect(gitlab_yml_data['production']['pages']['external_http']).to be true
+            expect(gitlab_yml_data['production']['pages']['external_https']).to be false
+          end
+        end
+
+        describe 'pages configuration' do
+          it 'does not expose listen-https or root-cert or root-key' do
+            expect(pages_config_data).not_to match(/listen-https=/)
+            expect(pages_config_data).not_to match(/root-cert=/)
+            expect(pages_config_data).not_to match(/root-key=/)
+          end
+
+          it 'does not expose listen-proxy ' do
+            expect(pages_config_data).not_to match(/listen-proxy=/)
+          end
+
+          it 'exposes listen-http correctly' do
+            expect(pages_config_data).to match(/listen-http=0.0.0.0:8090/)
+          end
+        end
+
+        describe 'pages-custom-domain service' do
+          it 'is enabled and exposes correct port' do
+            expect(pages_enabled_template.dig('Service/test-gitlab-pages-custom-domains', 'spec', 'ports')).to eq(
+              [
+                {
+                  'port' => 80,
+                  'targetPort' => 8090,
+                  'protocol' => 'TCP',
+                  'name' => 'http-gitlab-pages'
+                }
+              ]
+            )
+          end
+        end
+
+        describe 'pages TLS secret' do
+          it 'is not mounted on Pages pod' do
+            pages_secret_mounts = pages_enabled_template.projected_volume_sources(
+              'Deployment/test-gitlab-pages',
+              'init-pages-secrets'
+            )
+
+            tls_mount = pages_secret_mounts.find { |mount| mount['secret']['name'] == 'test-pages-tls' }
+
+            expect(tls_mount).to be_nil
+          end
+        end
+      end
+
+      context 'when only HTTPS custom domains are enabled' do
+        let(:pages_enabled_values) do
+          {
+            'global' => {
+              'pages' => {
+                'enabled' => true,
+                'externalHttps' => %w[1.2.3.4]
+              }
+            }
+          }
+        end
+
+        describe 'gitlab.yml file' do
+          it 'sets externalHTTP to true and externalHTTPS to false' do
+            expect(gitlab_yml_data['production']['pages']['external_http']).to be false
+            expect(gitlab_yml_data['production']['pages']['external_https']).to be true
+          end
+        end
+
+        describe 'pages configuration' do
+          it 'exposes listen-https, root-cert, and root-key' do
+            expect(pages_config_data).to match(/listen-https=0.0.0.0:8091/)
+            expect(pages_config_data).to match(%r{root-cert=/etc/gitlab-secrets/pages/pages.example.com.crt})
+            expect(pages_config_data).to match(%r{root-key=/etc/gitlab-secrets/pages/pages.example.com.key})
+          end
+
+          it 'does not expose listen-http' do
+            expect(pages_config_data).not_to match(/listen-http=/)
+          end
+
+          it 'does not expose listen-proxy ' do
+            expect(pages_config_data).not_to match(/listen-proxy=/)
+          end
+        end
+
+        describe 'pages-custom-domain service' do
+          it 'is enabled and exposes correct port' do
+            expect(pages_enabled_template.dig('Service/test-gitlab-pages-custom-domains', 'spec', 'ports')).to eq(
+              [
+                {
+                  'port' => 443,
+                  'targetPort' => 8091,
+                  'protocol' => 'TCP',
+                  'name' => 'https-gitlab-pages'
+                }
+              ]
+            )
+          end
+        end
+
+        describe 'pages TLS secret' do
+          it 'is mounted on Pages pod' do
+            pages_secret_mounts = pages_enabled_template.projected_volume_sources(
+              'Deployment/test-gitlab-pages',
+              'init-pages-secrets'
+            )
+
+            tls_mount = pages_secret_mounts.find { |mount| mount['secret']['name'] == 'test-pages-tls' }
+
+            expect(tls_mount).not_to be_nil
+          end
+        end
+      end
+
+      context 'when both HTTP and HTTPS custom domains are enabled' do
+        let(:pages_enabled_values) do
+          {
+            'global' => {
+              'pages' => {
+                'enabled' => true,
+                'externalHttp' => %w[1.2.3.4],
+                'externalHttps' => %w[1.2.3.4]
+              }
+            }
+          }
+        end
+
+        describe 'gitlab.yml file' do
+          it 'sets both externalHTTP and externalHTTPS to true' do
+            expect(gitlab_yml_data['production']['pages']['external_http']).to be true
+            expect(gitlab_yml_data['production']['pages']['external_https']).to be true
+          end
+        end
+
+        describe 'pages configuration' do
+          it 'exposes listen-http, listen-https, root-cert, and root-key' do
+            expect(pages_config_data).to match(/listen-http=0.0.0.0:8090/)
+            expect(pages_config_data).to match(/listen-https=0.0.0.0:8091/)
+            expect(pages_config_data).to match(%r{root-cert=/etc/gitlab-secrets/pages/pages.example.com.crt})
+            expect(pages_config_data).to match(%r{root-key=/etc/gitlab-secrets/pages/pages.example.com.key})
+          end
+
+          it 'does not expose listen-proxy ' do
+            expect(pages_config_data).not_to match(/listen-proxy=/)
+          end
+        end
+
+        describe 'pages-custom-domain service' do
+          it 'is enabled and exposes correct ports' do
+            expect(pages_enabled_template.dig('Service/test-gitlab-pages-custom-domains', 'spec', 'ports')).to eq(
+              [
+                {
+                  'port' => 80,
+                  'targetPort' => 8090,
+                  'protocol' => 'TCP',
+                  'name' => 'http-gitlab-pages'
+                },
+                {
+                  'port' => 443,
+                  'targetPort' => 8091,
+                  'protocol' => 'TCP',
+                  'name' => 'https-gitlab-pages'
+                }
+              ]
+            )
+          end
+        end
+
+        describe 'pages TLS secret' do
+          it 'is mounted on Pages pod' do
+            pages_secret_mounts = pages_enabled_template.projected_volume_sources(
+              'Deployment/test-gitlab-pages',
+              'init-pages-secrets'
+            )
+
+            tls_mount = pages_secret_mounts.find { |mount| mount['secret']['name'] == 'test-pages-tls' }
+
+            expect(tls_mount).not_to be_nil
+          end
+        end
+      end
+
+      describe 'custom domains service type' do
+        context 'when using LoadBalancer' do
+          context 'when only one unique IP address exists combined for both http and https' do
+            let(:pages_enabled_values) do
+              {
+                'global' => {
+                  'pages' => {
+                    'enabled' => true,
+                    'externalHttp' => %w[1.2.3.4],
+                    'externalHttps' => %w[1.2.3.4]
+                  }
+                }
+              }
+            end
+
+            it 'sets loadBalancerIP' do
+              expect(pages_enabled_template.dig('Service/test-gitlab-pages-custom-domains', 'spec', 'loadBalancerIP')).to eq('1.2.3.4')
+            end
+          end
+
+          context 'when more than one unique IP address exists combined for both http and https' do
+            let(:pages_enabled_values) do
+              {
+                'global' => {
+                  'pages' => {
+                    'enabled' => true,
+                    'externalHttp' => %w[1.2.3.4 1.2.3.5],
+                    'externalHttps' => %w[1.2.3.4 1.2.3.6]
+                  }
+                }
+              }
+            end
+
+            it 'sets externalIPs' do
+              expect(pages_enabled_template.dig('Service/test-gitlab-pages-custom-domains', 'spec', 'loadBalancerIP')).to be_nil
+              expect(pages_enabled_template.dig('Service/test-gitlab-pages-custom-domains', 'spec', 'externalIPs')).to eq(%w[1.2.3.4 1.2.3.5 1.2.3.6])
+            end
+          end
+        end
+
+        context 'when using NodePort' do
+          let(:pages_enabled_values) do
+            {
+              'global' => {
+                'pages' => {
+                  'enabled' => true,
+                  'externalHttp' => %w[1.2.3.4],
+                  'externalHttps' => %w[1.2.3.4]
+                }
+              },
+              'gitlab' => {
+                'gitlab-pages' => {
+                  'service' => {
+                    'customDomains' => {
+                      'type' => 'NodePort',
+                      'nodePort' => {
+                        'http' => 30010,
+                        'https' => 30011
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          end
+
+          it 'sets NodePort as service type' do
+            expect(pages_enabled_template.dig('Service/test-gitlab-pages-custom-domains', 'spec', 'type')).to eq('NodePort')
+          end
+
+          it 'exposes node ports' do
+            expect(pages_enabled_template.dig('Service/test-gitlab-pages-custom-domains', 'spec', 'ports')).to eq(
+              [
+                {
+                  'port' => 80,
+                  'targetPort' => 8090,
+                  'protocol' => 'TCP',
+                  'name' => 'http-gitlab-pages',
+                  'nodePort' => 30010
+                },
+                {
+                  'port' => 443,
+                  'targetPort' => 8091,
+                  'protocol' => 'TCP',
+                  'name' => 'https-gitlab-pages',
+                  'nodePort' => 30011
+                }
+              ]
+            )
+          end
         end
       end
     end
