727ca996f196d2a1db62279f8b885e6ca9376c42
Marshall Cottrell <marshall.cottrell@nasa.gov>
2021-02-24 15:11:16 -0600

    fix omissions, update tests to include other resource types

--- spec/configuration/node_selector_spec.rb
+++ spec/configuration/node_selector_spec.rb
@@ -8,8 +8,14 @@ describe 'Node Selector configuration' do
   let(:default_values) do
     {
       'certmanager-issuer' => { 'email' => 'test@example.com' },
-      'gitlab' => { 'kas' => { 'enabled' => 'true' } }, # DELETE THIS WHEN KAS BECOMES ENABLED BY DEFAULT
-      'global' => { 'nodeSelector' => { 'region' => 'us-central-1a' } }
+      'global' => {
+        'gitlab' => {
+          'kas' => { 'enabled' => 'true' }, # DELETE THIS WHEN KAS BECOMES ENABLED BY DEFAULT
+          'pages' => { 'enabled' => 'true' },
+          'praefect' => { 'enabled' => 'true' }
+        },
+        'nodeSelector' => { 'region' => 'us-central-1a' }
+      }
     }
   end
 
@@ -18,18 +24,25 @@ describe 'Node Selector configuration' do
       'Deployment/test-cainjector',
       'Deployment/test-cert-manager',
       'Deployment/test-gitlab-runner',
-      'Deployment/test-prometheus-server'
+      'Deployment/test-prometheus-server',
+      'StatefulSet/test-postgresql',
+      'StatefulSet/test-redis-master'
     ]
   end
 
   context 'When setting global nodeSelector' do
-    it 'Populates nodeSelector for all deployments' do
+    it 'Populates nodeSelector for all resources' do
       t = HelmTemplate.new(default_values)
       expect(t.exit_code).to eq(0)
 
-      resources_by_kind = t.resources_by_kind('Deployment').reject { |key, _| ignored_charts.include? key }
+      resources = [
+        *t.resources_by_kind('Deployment'),
+        *t.resources_by_kind('DaemonSet'),
+        *t.resources_by_kind('StatefulSet')
+      ]
+      .to_h.reject { |key, _| ignored_charts.include? key }
 
-      resources_by_kind.each do |key, _|
+      resources.each do |key, _|
         expect(t.dig(key, 'spec', 'template', 'spec', 'nodeSelector')).to include(default_values['global']['nodeSelector']), key
       end
     end
