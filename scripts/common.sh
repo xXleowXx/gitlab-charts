8313f60f5a2c15c4e881e06c0b799a22a40cb118
Gerard Hickey <ghickey@gitlab.com>
2021-03-24 14:41:31 -0700

    refactor: simplification of semver_compare()

--- scripts/common.sh
+++ scripts/common.sh
@@ -92,57 +92,20 @@ function cluster_admin_password_gke(){
 }
 
 # Function to compare versions in a semver compatible way
-# https://gist.github.com/Ariel-Rodriguez/9e3c2163f4644d7a389759b224bfe7f3
-# Author Ariel Rodriguez
-# License MIT
+# given args A and B, return 0 if A=B, -1 if A<B and 1 if A>B
 function semver_compare() {
-  local version_a version_b pr_a pr_b
-  # strip word "v" and extract first subset version (x.y.z from x.y.z-foo.n)
-  version_a=$(echo "${1//v/}" | awk -F'-' '{print $1}')
-  version_b=$(echo "${2//v/}" | awk -F'-' '{print $1}')
+  if [ "$1" = "$2" ]; then
+    # A = B
+    echo 0
+  else
+    ordered=$(printf '%s\n' "$@" | sort -V | head -n 1)
 
-  if [ "$version_a" \= "$version_b" ]
-  then
-    # check for pre-release
-    # extract pre-release (-foo.n from x.y.z-foo.n)
-    pr_a=$(echo "$1" | awk -F'-' '{print $2}')
-    pr_b=$(echo "$2" | awk -F'-' '{print $2}')
-
-    ####
-    # Return 0 when A is equal to B
-    [ "$pr_a" \= "$pr_b" ] && echo 0 && return 0
-
-    ####
-    # Return 1
-
-    # Case when A is not pre-release
-    if [ -z "$pr_a" ]
-    then
-      echo 1 && return 0
+    if [ "$ordered" = "$1" ]; then
+      # A < B
+      echo -1
+    else
+      # A > B
+      echo 1
     fi
-
-    ####
-    # Case when pre-release A exists and is greater than B's pre-release
-
-    # extract numbers -rc.x --> x
-    number_a=$(echo ${pr_a//[!0-9]/})
-    number_b=$(echo ${pr_b//[!0-9]/})
-    [ -z "${number_a}" ] && number_a=0
-    [ -z "${number_b}" ] && number_b=0
-
-    [ "$pr_a" \> "$pr_b" ] && [ -n "$pr_b" ] && [ "$number_a" -gt "$number_b" ] && echo 1 && return 0
-
-    ####
-    # Retrun -1 when A is lower than B
-    echo -1 && return 0
   fi
-  arr_version_a=(${version_a//./ })
-  arr_version_b=(${version_b//./ })
-  cursor=0
-  # Iterate arrays from left to right and find the first difference
-  while [ "$([ "${arr_version_a[$cursor]}" -eq "${arr_version_b[$cursor]}" ] && [ $cursor -lt ${#arr_version_a[@]} ] && echo true)" == true ]
-  do
-    cursor=$((cursor+1))
-  done
-  [ "${arr_version_a[$cursor]}" -gt "${arr_version_b[$cursor]}" ] && echo 1 || echo -1
 }
