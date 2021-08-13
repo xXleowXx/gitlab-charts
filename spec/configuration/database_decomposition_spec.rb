c96ba2342b61534cc00a89a638523747a5d44220
Jason Plum <jplum@gitlab.com>
2021-08-13 17:18:49 -0400

    Further RSpec

--- spec/configuration/database_decomposition_spec.rb
+++ spec/configuration/database_decomposition_spec.rb
@@ -37,25 +37,39 @@ describe 'Database configuration' do
         install: true
     )))
   end
-
-  let(:decompose_ci) do
-    default_values.deep_merge(YAML.safe_load(%(
-      global:
-        psql:
-          ci:
-            foo: bar
-    )))
-  end
-
   
-  describe 'No decomposition (defaults)' do
-    context 'database.yml' do
-      it 'Provides only `main` stanza, using in-chart postgresql service' do
+  describe 'No decomposition' do
+    context 'default configuration' do
+      it '`database.yml` Provides only `main` stanza and uses in-chart postgresql service' do
         t = HelmTemplate.new(default_values)
         expect(t.exit_code).to eq(0), "Unexpected error code #{t.exit_code} -- #{t.stderr}"
         db_config = database_config(t,'webservice')
         expect(db_config['production'].keys).to contain_exactly('main')
         expect(db_config['production'].dig('main','host')).to eq('test-postgresql.default.svc')
+      end
+    end
+
+    context '`main` is provided' do
+      it 'inherits settings from x.psql where not provided, uses own' do
+        t = HelmTemplate.new(default_values.deep_merge(YAML.safe_load(%(
+          global:
+            psql:
+              password:
+                secret: sekrit
+                key: pa55word
+              main:
+                host: server
+                port: 9999
+        ))))
+
+        db_config = database_config(t,'webservice')
+        expect(db_config['production'].dig('main','host')).to eq('server')
+        expect(db_config['production'].dig('main','port')).to eq(9999)
+        
+        webservice_secret_mounts =  t.projected_volume_sources('Deployment/test-webservice-default','init-webservice-secrets').select { |item|
+          item['secret']['name'] == 'sekrit' && item['secret']['items'][0]['key'] == 'pa55word'
+        }
+        expect(webservice_secret_mounts.length).to eq(1)
       end
     end
   end
@@ -78,17 +92,44 @@ describe 'Database configuration' do
         expect(db_config['production'].keys).not_to include('bogus')
       end
     end
+
+    context 'volumes' do
+      it 'Does not template password files for `bogus` stanza' do
+        t = HelmTemplate.new(decompose_bogus)
+        expect(t.exit_code).to eq(0), "Unexpected error code #{t.exit_code} -- #{t.stderr}"
+        webservice_secret_mounts =  t.projected_volume_sources('Deployment/test-webservice-default','init-webservice-secrets').select { |item|
+          item['secret']['items'][0]['key'] == 'postgresql-password' && item['secret']['items'][0]['path'] == 'postgres/psql-password-bogus'
+        }
+        expect(webservice_secret_mounts.length).to eq(0)
+      end
+    end
   end
   
   describe 'CI is decomposed (x.psql.ci)' do
-    context 'database.yml' do
-      it 'Provides `ci` stanza' do
+    let(:decompose_ci) do
+      default_values.deep_merge(YAML.safe_load(%(
+        global:
+          psql:
+            ci:
+              foo: bar
+      )))
+    end
+
+    context 'minimal configuration' do
+      it 'Provides `main` and `ci` stanzas' do
         t = HelmTemplate.new(decompose_ci)
         expect(t.exit_code).to eq(0), "Unexpected error code #{t.exit_code} -- #{t.stderr}"
         db_config = database_config(t,'webservice')
         expect(db_config['production'].keys).to include('main','ci')
         expect(db_config['production'].dig('main','host')).to eq('test-postgresql.default.svc')
         expect(db_config['production'].dig('ci','host')).to eq('test-postgresql.default.svc')
+      end
+
+      it 'Templates different password files for each stanza' do
+        t = HelmTemplate.new(decompose_ci)
+        expect(t.exit_code).to eq(0), "Unexpected error code #{t.exit_code} -- #{t.stderr}"
+        database_yml = database_yml(t,'webservice')
+        expect(database_yml).to include('/etc/gitlab/postgres/psql-password-main','/etc/gitlab/postgres/psql-password-ci')
       end
     end
   end
