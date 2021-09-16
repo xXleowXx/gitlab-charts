fac1aee26a2a34e70f7f2b94ca05032b265cd69d
Balasankar "Balu" C <balasankar@gitlab.com>
2021-09-16 16:02:04 +0530

    Add code to create an operator MR to update CHART_VERSIONS file
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

---
+++ scripts/create_operator_mr.sh
@@ -0,0 +1,23 @@
+#!/bin/env bash
+git config --global user.name "GitLab Distribution"
+git config --global user.email "distribution-be@gitlab.com"
+OPERATOR_PROJECT=${OPERATOR_PROJECT:-gitlab.com/gitlab-org/cloud-native/gitlab-operator.git}
+
+rm -rf /tmp/gitlab-operator
+git clone "https://${OPERATOR_PROJECT_USERNAME}:${OPERATOR_PROJECT_PASSWORD}@${OPERATOR_PROJECT}" /tmp/gitlab-operator
+
+# Update the CHART_VERSIONS file
+bundle exec ruby -e "require './scripts/gitlab_charts_helper'; puts GitLabChartsHelper.supported_versions" > /tmp/gitlab-operator/CHART_VERSIONS
+
+pushd /tmp/gitlab-operator || exit
+  git checkout -b "bump-charts-${CI_COMMIT_TAG}"
+  git add CHART_VERSIONS
+  git commit -m "Update CHART_VERSIONS for GitLab Chart release ${CI_COMMIT_TAG}"
+  git push -f origin "bump-charts-${CI_COMMIT_TAG}" \
+    -o merge_request.create \
+    -o merge_request.label="group::distribution" \
+    -o merge_request.label="devops::enablement" \
+    -o merge_request.label="section::enablement" \
+    -o merge_request.label="feature::maintenance" \
+    -o merge_request.label="Category:Cloud Native Installation"
+popd || exit
