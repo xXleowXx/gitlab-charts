037dd406ebf2f1a07a2f73e0518a62883d601d46
Dmitry Makovey <dmakovey@gitlab.com>
2021-10-28 14:12:14 -0600

    update tests

--- spec/configuration/database_spec.rb
+++ spec/configuration/database_spec.rb
@@ -266,7 +266,7 @@ describe 'Database configuration' do
           .and match(/keepalives_interval: $/)
           .and match(/keepalives_count: $/)
           .and match(/tcp_user_timeout: $/)
-        sidekiq_secret_mounts =  t.projected_volume_sources('Deployment/test-sidekiq-all-in-1-v1','init-sidekiq-secrets').select { |item|
+        sidekiq_secret_mounts =  t.projected_volume_sources('Deployment/test-sidekiq-all-in-1-v2','init-sidekiq-secrets').select { |item|
           item['secret']['name'] == 'test-postgresql-password'
         }
         expect(sidekiq_secret_mounts.length).to eq(1)
@@ -320,7 +320,7 @@ describe 'Database configuration' do
           .and match(/keepalives_interval: $/)
           .and match(/keepalives_count: $/)
           .and match(/tcp_user_timeout: $/)
-        sidekiq_secret_mounts =  t.projected_volume_sources('Deployment/test-sidekiq-all-in-1-v1','init-sidekiq-secrets').select { |item|
+        sidekiq_secret_mounts =  t.projected_volume_sources('Deployment/test-sidekiq-all-in-1-v2','init-sidekiq-secrets').select { |item|
           item['secret']['name'] == 'test-postgresql-password' && item['secret']['items'][0]['key'] == 'postgresql-password'
         }
         expect(sidekiq_secret_mounts.length).to eq(1)
@@ -366,7 +366,7 @@ describe 'Database configuration' do
         expect(t.exit_code).to eq(0)
         # sidekiq gets "global"
         expect(t.dig('ConfigMap/test-sidekiq','data','database.yml.erb')).to include('host: "psql.global"')
-        sidekiq_secret_mounts =  t.projected_volume_sources('Deployment/test-sidekiq-all-in-1-v1','init-sidekiq-secrets').select { |item|
+        sidekiq_secret_mounts =  t.projected_volume_sources('Deployment/test-sidekiq-all-in-1-v2','init-sidekiq-secrets').select { |item|
           item['secret']['name'] == 'global-postgresql-password' && item['secret']['items'][0]['key'] == 'global-password'
         }
         expect(sidekiq_secret_mounts.length).to eq(1)
