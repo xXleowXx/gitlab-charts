ae2616448499a614fed1e044019170ca12822e55
Mitchell Nielsen <mnielsen@gitlab.com>
2022-02-09 19:41:51 -0600

    Reorder tests for legibility

--- spec/configuration/ingress_spec.rb
+++ spec/configuration/ingress_spec.rb
@@ -314,12 +314,12 @@ describe 'GitLab Ingress configuration(s)' do
       where(:api_version, :class_name, :expected_annotation, :expected_spec) do
         'v1beta1' | nil    | 'test-nginx' | nil
         'v1beta1' | 'foo'  | 'foo'        | nil
-        'v1beta1' | 'none' | nil          | nil
         'v1beta1' | '""'   | ""           | nil
+        'v1beta1' | 'none' | nil          | nil
         'v1'      | nil    | nil          | 'test-nginx'
         'v1'      | 'foo'  | nil          | 'foo'
-        'v1'      | 'none' | nil          | nil
         'v1'      | '""'   | nil          | ""
+        'v1'      | 'none' | nil          | nil
       end
 
       with_them do
