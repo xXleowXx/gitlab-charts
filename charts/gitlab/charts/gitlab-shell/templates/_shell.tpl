2bcaadd563ff9a9c5d7cbf77edb368562927aef6
Stan Hu <stanhu@gmail.com>
2022-05-09 14:05:52 -0700

    Set nginx-ingress TCP settings for PROXY protocol
    If the PROXY protocol is enabled for gitlab-sshd, we also
    have to adjust the nginx-ingress settings to handle this.
    
    If nginx-ingress is handling inbound SSH traffic and gitlab-sshd
    expects the PROXY protocol, we have to ensure that NGINX also speaks
    this protocol on the backend or SSH connections will fail to
    establish. As described in
    https://kubernetes.github.io/ingress-nginx/user-guide/exposing-tcp-udp-services/
    this is handled by defining the TCP map:
    
    ```
    <namespace/service name>:<service port>:[PROXY]:[PROXY]
    ```
    
    The first optional `PROXY` field causes NGINX to speak PROXY on the
    inbound port, which is already enabld by
    `global.shell.tcp.proxyProtocol`.
    
    The second optional `PROXY` field causes NGINX to emit the
    `proxy_protocol on` setting on the backend.
    
    This commit adds support for all four permutations and adds tests.
    
    Changelog: changed

---
+++ charts/gitlab/charts/gitlab-shell/templates/_shell.tpl
@@ -0,0 +1,24 @@
+{{/*
+Return the string 'PROXY'
+
+The string 'PROXY' ensures the use of ProxyProtocol decoding in a TCP service.
+This string is exactly compared with the string 'PROXY' when using nginx-ingress (in capital letters).
+See: https://kubernetes.github.io/ingress-nginx/user-guide/exposing-tcp-udp-services/
+*/}}
+{{- define "gitlab.shell.tcp.proxyProtocol" -}}
+{{- $inbound := "" -}}
+{{- if .Values.global.shell.tcp.proxyProtocol -}}
+{{-   $inbound = "PROXY" -}}
+{{- end -}}
+{{- $outbound := "" -}}
+{{- if .Values.config.proxyProtocol -}}
+{{-   $outbound = "PROXY" -}}
+{{- end -}}
+{{- if (not (empty $inbound)) -}}
+:{{ $inbound }}
+{{- end -}}
+{{- if (not (empty $outbound)) -}}
+{{-   if (empty $inbound) -}}:{{- end -}}
+:{{ $outbound }}
+{{- end -}}
+{{- end -}}
--- spec/configuration/gitlab_shell_spec.rb
+++ spec/configuration/gitlab_shell_spec.rb
@@ -4,6 +4,7 @@ require 'yaml'
 require 'hash_deep_merge'
 
 describe 'gitlab-shell configuration' do
+  let(:t) { HelmTemplate.new(values) }
   let(:default_values) do
     YAML.safe_load(%(
       certmanager-issuer:
@@ -17,6 +18,76 @@ describe 'gitlab-shell configuration' do
             enabled: true
             create: true
     ))
+  end
+
+  def expect_successful_exit_code
+    expect(t.exit_code).to eq(0), "Unexpected error code #{t.exit_code} -- #{t.stderr}"
+  end
+
+  context 'when gitlab-sshd is enabled' do
+    let(:proxy_protocol) { true }
+    let(:values) do
+      YAML.safe_load(%(
+        gitlab:
+          gitlab-shell:
+            sshDaemon: "gitlab-sshd"
+            config:
+              proxyProtocol: #{proxy_protocol}
+      )).deep_merge(default_values)
+    end
+
+    shared_examples 'gitlab-sshd config' do
+      let(:config) { t.dig('ConfigMap/test-gitlab-shell', 'data', 'config.yml.tpl') }
+
+      it 'renders gitlab-sshd config' do
+        expect_successful_exit_code
+        expect(config).to match(/^sshd:$/)
+        expect(config).to include("proxy_protocol: #{proxy_protocol}")
+      end
+    end
+
+    it_behaves_like 'gitlab-sshd config'
+
+    context 'when proxyProtocol is disabled' do
+      let(:proxy_protocol) { false }
+
+      it_behaves_like 'gitlab-sshd config'
+    end
+  end
+
+  context 'when PROXY protocol is set' do
+    using RSpec::Parameterized::TableSyntax
+
+    where(:in_proxy_protocol, :out_proxy_protocol, :expected_suffix) do
+      false | false | ""
+      true  | false | ":PROXY"
+      true  | true  | ":PROXY:PROXY"
+      false | true  | "::PROXY"
+    end
+
+    with_them do
+      let(:values) do
+        YAML.safe_load(%(
+          global:
+            shell:
+              tcp:
+                proxyProtocol: #{in_proxy_protocol}
+          gitlab:
+            gitlab-shell:
+              config:
+                proxyProtocol: #{out_proxy_protocol}
+        )).deep_merge(default_values)
+      end
+
+      it 'should render NGINX ingress TCP data correctly' do
+        expect_successful_exit_code
+
+        data = t.dig('ConfigMap/test-nginx-ingress-tcp', 'data')
+
+        expect(data.keys).to eq(['22'])
+        expect(data['22']).to eq("default/test-gitlab-shell:22#{expected_suffix}")
+      end
+    end
   end
 
   context 'When customer provides additional labels' do
@@ -47,9 +118,10 @@ describe 'gitlab-shell configuration' do
               global: service
       )).deep_merge(default_values)
     end
+
     it 'Populates the additional labels in the expected manner' do
-      t = HelmTemplate.new(values)
-      expect(t.exit_code).to eq(0), "Unexpected error code #{t.exit_code} -- #{t.stderr}"
+      expect_successful_exit_code
+
       expect(t.dig('ConfigMap/test-gitlab-shell', 'metadata', 'labels')).to include('global' => 'shell')
       expect(t.dig('Deployment/test-gitlab-shell', 'metadata', 'labels')).to include('foo' => 'global')
       expect(t.dig('Deployment/test-gitlab-shell', 'metadata', 'labels')).to include('global' => 'shell')
