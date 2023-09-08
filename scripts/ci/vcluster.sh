98c0e474ed320dc462c2fa9f04fb8f77f57b75b0
Mitchell Nielsen <mnielsen@gitlab.com>
2023-09-07 20:12:00 -0500

    Disable vcluster expose, run commands directly
    Runs commands passed to `vcluster connect` instead of having
    to expose the cluster via a LoadBalancer.

--- scripts/ci/vcluster.sh
+++ scripts/ci/vcluster.sh
@@ -11,7 +11,6 @@ function cluster_connect() {
 
 function vcluster_create() {
   vcluster create ${VCLUSTER_NAME} \
-    --expose \
     --upgrade \
     --namespace=${VCLUSTER_NAME} \
     --kubernetes-version=${VCLUSTER_K8S_VERSION} \
@@ -19,69 +18,17 @@ function vcluster_create() {
     --update-current=false
 }
 
-function vcluster_connect() {
-  vcluster connect ${VCLUSTER_NAME}
+function vcluster_run() {
+  vcluster connect ${VCLUSTER_NAME} -- $@
 }
 
 function vcluster_deploy() {
   helm dependency update
 
-  cat << CIVALUES > ci.yaml
-  global:
-    hosts:
-      https: false
-    image:
-      pullPolicy: Always
-    ingress:
-      configureCertmanager: false
-      tls:
-        enabled: false
-    appConfig:
-      initialDefaults:
-        signupEnabled: false
-  gitlab:
-    webservice:
-      minReplicas: 1    # 2
-      maxReplicas: 3    # 10
-      resources:
-        requests:
-          cpu: 500m     # 900m
-          memory: 1500M # 2.5G
-    sidekiq:
-      minReplicas: 1    # 1
-      maxReplicas: 2    # 10
-      resources:
-        requests:
-          cpu: 500m     # 900m
-          memory: 1000M # 2G
-    gitlab-shell:
-      minReplicas: 1    # 2
-      maxReplicas: 2    # 10
-    toolbox:
-      enabled: true
-  gitlab-runner:
-    certsSecretName: gitlab-wildcard-tls-chain
-  certmanager:
-    install: false
-  nginx-ingress:
-    controller:
-      replicaCount: 1   # 2
-      service:
-        type: NodePort # to avoid creating a LoadBalancer
-  redis:
-    resources:
-      requests:
-        cpu: 100m
-  minio:
-    resources:
-      requests:
-        cpu: 100m
-CIVALUES
-
-  helm upgrade --install \
+  vcluster_run helm upgrade --install \
     gitlab \
     --wait --timeout 600s \
-    -f ci.yaml \
+    -f ./scripts/ci/vcluster_helm_values.yaml \
     .
 }
 
