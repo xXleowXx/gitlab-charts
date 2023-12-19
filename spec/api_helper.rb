693f3f9d9f7c3fe2a50b105804173780a54564bd
vishal.s.patel <vpatel@gitlab.com>
2023-12-20 12:18:58 +1300

    adding api helper

---
+++ spec/api_helper.rb
@@ -0,0 +1,11 @@
+require 'net/http'
+
+module ApiHelper
+
+        def self.invoke_http_request(uri, request)
+            response = Net::HTTP.start(uri.hostname, uri.port) {|http|
+                http.request(request)
+            }
+        end
+        
+end
