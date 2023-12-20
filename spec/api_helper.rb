fc47ee987ecb29d4a7ab196d305badb0df448b89
vishal.s.patel <vpatel@gitlab.com>
2023-12-21 11:07:38 +1300

    Changed hash to new syntax

--- spec/api_helper.rb
+++ spec/api_helper.rb
@@ -2,7 +2,7 @@ require 'net/http'
 
 module ApiHelper
   def self.invoke_http_request(uri, request)
-    Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
+    Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
       http.request(request)
     end
   end
