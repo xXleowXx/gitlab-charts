138b7809360021c0b28dedc026fd9e7ca0a4de10
vishal.s.patel <vpatel@gitlab.com>
2023-12-20 13:12:14 +1300

    added space

--- spec/api_helper.rb
+++ spec/api_helper.rb
@@ -1,9 +1,8 @@
 require 'net/http'
 
 module ApiHelper
-    def self.invoke_http_request(uri, request)
-        response = Net::HTTP.start(uri.hostname, uri.port) { |http|
-            http.request(request)
-        }
-    end
+  def self.invoke_http_request(uri, request)
+    Net::HTTP.start(uri.hostname, uri.port) |http|
+      http.request(request)
+  end
 end
