0aed8cf594bd0b4636c72fbda5aabcffb1c1db5c
vishal.s.patel <vpatel@gitlab.com>
2024-01-24 11:13:23 +1300

    Updating debug code

--- spec/api_helper.rb
+++ spec/api_helper.rb
@@ -2,8 +2,8 @@ require 'rest-client'
 require 'json'
 
 module ApiHelper
-  BASE_URL = "https://#{ENV['QA_ENVIRONMENT_URL']}/api/v4/".freeze
-
+  # BASE_URL = "https://#{ENV['QA_ENVIRONMENT_URL']}/api/v4/".freeze
+  BASE_URL= "http://gdk.test:3000/api/v4/".freeze
   def self.invoke_get_request(uri)
     puts "######################"
     puts BASE_URL.to_s
@@ -16,8 +16,8 @@ module ApiHelper
       }
     }
     response = RestClient::Request.execute(default_args)
-    JSON.parse(response.body)
     puts "######################"
     puts JSON.parse(response.body).to_s
+    JSON.parse(response.body)
   end
 end
