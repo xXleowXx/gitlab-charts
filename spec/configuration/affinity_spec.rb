8354782598824f02dd41bf2ecb12c6d2763eab51
Shreya Shah <sshah@gitlab.com>
2024-06-11 17:29:34 +0530

    Test file updated

--- spec/configuration/affinity_spec.rb
+++ spec/configuration/affinity_spec.rb
@@ -4,14 +4,14 @@ require 'yaml'
 require 'hash_deep_merge'
 
 SUPPORTED_NODE_AFFINITY_DEPLOYMENTS = %w[
-Deployment/test-registry,
-Deployment/test-toolbox,
-Deployment/test-kas,
-Deployment/test-gitlab-shell,
-Deployment/test-gitlab-exporter,
-Deployment/test-gitlab-shell,
-Deployment/test-toolbox,
-StatefulSet/test-gitaly
+  Deployment/test-registry
+  Deployment/test-toolbox
+  Deployment/test-kas
+  Deployment/test-gitlab-shell
+  Deployment/test-gitlab-exporter
+  Deployment/test-gitlab-shell
+  Deployment/test-toolbox
+  StatefulSet/test-gitaly
 ].freeze
 IGNORED_DEPLOYMENTS = [
   'Deployment/test-certmanager',
